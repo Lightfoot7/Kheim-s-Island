a5b0173084c4a3ac1401bfd574438faaf9d07c4c
Lightfoot7 <ammargalal444@gmail.com>
2026-03-22 18:26:08 +0000

    3rd_Final_VIRGIN

--- TicTacFeeet.cpp
+++ TicTacFeeet.cpp
@@ -3,133 +3,144 @@
 using namespace std;
 
 
+class TicTacToe_Game{
+private:
+    // Member variables will be defined here
+    char Current_Player;
+    // The game's internal state is protected now
+    vector<vector<char>> map;
+
+
+
+public: 
+    // Create a constructor to initialize the game
+    TicTacToe_Game(){
+        // Initialise the game board
+        map = vector<vector<char>>(3, vector<char>(3, '.'));
+        // Initialise the current player
+        Current_Player = 'X';
+
+    }   
+    // Functions will be defined here
+    // Prints the game board
+    void Print_Arena() const {
+    cout << "Enter your move (1-9): " << endl; cout << "GO GO GO YOUR TURN! Player: " << Current_Player << endl;
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                cout << map[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    //
+    // Check if the player ('X' or 'O') has won the game
+    // Just breaks the line in 2 for better readability    
+    //function that returns True if all elements in an iterable.
+    //The all() function is used to check whether certain conditions all conditions are met
+
+
+    bool Check_Winner(){
+        for (int i = 0; i < 3; i++){
+            // Check horizontal lines
+            if(map[i][0] == Current_Player && map[i][1] == Current_Player && map[i][2] == Current_Player){
+                return true;
+            }
+
+            // Check vertical lines
+            else if(map[0][i] == Current_Player && map[1][i] == Current_Player && map[2][i] == Current_Player){
+                return true;
+            }               
+        }
+        // Check diagonal lines
+        if(map[0][0]== Current_Player && map[1][1] == Current_Player && map[2][2] == Current_Player){
+            return true;
+        }
+   
+        // Check antidiagonal lines
+        if(map[0][2]== Current_Player && map[1][1] == Current_Player && map[2][0] == Current_Player){
+            return true;
+        }   
+
+        // If no winner is found, return false
+        return false;  
+    }
+
+
+
+    bool Is_Board_Full(){
+        // Check if the board is full (no empty spots), which is a Draw
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if(map[i][j] == '.'){
+                    // If no draw, return false
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+
+    void Switch_Player(){
+        if (Current_Player == 'X'){
+            Current_Player = 'O';
+        }
+        else if (Current_Player == 'O'){
+                Current_Player = 'X';
+            }
+        }
+
+    void Player_Move(){
+        while(true){
+            // Get a valid move from the player
+            int move; 
+            cin >> move; move--;  // Decrement move to convert to indexing format
+            int row = move / 3; 
+            int col = move % 3;      
+
+       
+            if (move < 0 || move > 8 || cin.fail()){
+                cout << "Invalid move broski. Please enter a number between 1 and 9." << endl;
+                cin.clear(); // Clear the error flag and reset cin, wihtout this cin will always fail
+                cin.ignore(420, '\n'); // Ignore the old bad input, or cin will try to read the same input again, we skip 420 charaters and \n tells the compiler to stop skippping once the end of the lines is reached where the user pressed the Enter button
+                Print_Arena();
+       }
+            else if (map[row][col] == '.'){
+                map[row][col] = Current_Player;
+                return;
+            }
+            else if (map[row][col] != '.'){
+                cout << "This spot is already taken you fool. Try again." << endl;
+                Print_Arena();
+            }
+
+        } 
+    }
+};
+
 int main(){
-    cout << "Welcome to Tic Tac Toe!" << endl;
-    cout << "Player 1 is X and Player 2 is O" << endl;
-    cout << "To make a move, enter a number between 1 and 9 corresponding to the board position:" << endl;
-    cout << "1 | 2 | 3" << endl;
-    cout << "4 | 5 | 6" << endl;
-    cout << "7 | 8 | 9" << endl;
+    TicTacToe_Game Kheim_TicTacToe;
+    while(true){
+        // Print the game board
+        Kheim_TicTacToe.Print_Arena();
+        // Get Player move, check if he wins
+        Kheim_TicTacToe.Player_Move();
 
-    // Create an instance of the TicTacToe_Game class and start the game
-    //TicTacToe_Game game;
-    //game.Play();
+        if(Kheim_TicTacToe.Check_Winner()){
+        Kheim_TicTacToe.Print_Arena();
+        cout << "WINNER WINNER CHICKEN DINNER!!! WOHOOOOO!! YOU ARE HIM, EVEN BETTER THAN KHEIM!" << endl;
+        break;
+        }
 
+        // Check for a draw, if not then switch palyer
+        if(Kheim_TicTacToe.Is_Board_Full()){
+        Kheim_TicTacToe.Print_Arena();
+        cout << "It's a Draw! BORIIING!" << endl;
+        break;
+        };
+
+        Kheim_TicTacToe.Switch_Player();
+
+    }
+    
     return 0;
 }
-/*
-class TicTacToe_Game{
-
-private:
-
-public: // test
-
-// Prints the game board
-void Create_Arena() {
-    vector<vector<char>> map(3, vector<char>(3, '.'));
-   
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            cout << map[i][j] << " ";
-        }
-        cout << endl;
-    }
-    Current_Player = 'X';
-    cout << "PLAY YOUR FIRST MOVE BITCH!" << endl;
-}
-//
-// Check if the player ('X' or 'O') has won the game
-// Just breaks the line in 2 for better readability    
-//function that returns True if all elements in an iterable.
-//The all() function is used to check whether certain conditions all conditions are met
-void Check_Winner(){
-    // Check horizontal and vertical lines
-    for (int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            if(map[i][j] == Current_Player || map[i][j] == Current_Player){
-                cout << "YOU WIN BUDDY!" << endl;
-                return true;
-            }
-            // Check diagonal lines
-            else if(map[i][i] == Current_Player || map[i][2-i] == Current_Player){
-                cout << "YOU WIN BUDDY!" << endl;
-                return true;
-            }                      
-        }
-        //return false    
-    }
-}
-
-void Is_Board_Full(){
-    // Check if the board is full (no empty spots), which is a Draw
-        if self.is_full():
-        self.print_board()
-   
-        cout << "It's a draw! BORIIING" << endl;
-        break
-
-    // nested loop
-    return all([map[i][j] != '.' for i in range(3) for j in range(3)])  
-}
-
-void Switch_Player(){
-    if (Current_Player == 'X'){
-        Current_Player = 'O';
-        else if (Current_Player == 'O'){
-            Current_Player = 'X';
-        }
-    }
-
-void Player_Move(){
-    // Get a valid move from the player
-        while(True){
-            int move; cout << "Enter your move (1-9): " << endl; cin >> move; move--;
-                row, col = divmod(move, 3)      
- //Takes dividend, divisor and gives quotient, remainder.
- //Floor division, remainder = move - (quotient*dividend).
-            if (map[row][col] == '.'){
-                map[row][col] = 'X';
-                break;
-            }
-            else{
-                cout << "This spot is already taken. Try again." << endl;
-            }
-            except (ValueError, IndexError):                                  # try this code, except if u get these errors, value: a, indec: int > 9
-                print("Invalid move. Please enter a number between 1 and 9.")
-        }
-}
-
-void Play(){
-    while(True){
-
-        // Clear the previous output
-    // Clear_output();
-
-        // Print the game
-        Create_Arena();
-           
-        // PLAYER 1 move
-        cout << "Player 1's turn" << endl;
-
-        if Current_Player == 'X':
-        self.human_move()
-        if self.is_winner('X'):
-        self.print_board()
-        cout << "YOU WINNNNNNNN LOSER!" << endl;
-        break
-        Current_Player = 'O'
-
-        // PLAYER 2 move
-        cout << "Player 2's turn" << endl;
-        if Current_Player == 'O':
-        self.computer_move()
-        if self.is_winner('O'):
-        self.print_board()
-        cout << "YOU LOST TO A 95 LINE CODE XD HAHAHAAHAHAHAHAHAHAHAHAHAHAHAAHAHAHA!" << endl;
-        break;
-        Current_Player = 'X';
-    }
- }
- }
-
-} */
